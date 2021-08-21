482b069159610e717f4e502df08236d5a4a7c49c
jeonghoon jeon <shu7081@gmail.com>
2021-08-22 07:33:49 +0900

    v1.1

---
+++ src/components/App.js
@@ -0,0 +1,27 @@
+import { useState, useEffect } from "react";
+import AppRouter from "components/Router";
+import { authService } from "fbase";
+
+function App() {
+  const [init, setInit] = useState(false);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  useEffect(() => {
+    authService.onAuthStateChanged((user) => {
+      if (user) {
+        setIsLoggedIn(user);
+      } else {
+        setIsLoggedIn(false);
+      }
+      setInit(true);
+    });
+  }, []);
+
+  return (
+    <>
+      {init ? <AppRouter isLoggedIn={isLoggedIn} /> : "initalizing.."}
+      <footer> &copy; {new Date().getFullYear()} Nwitter </footer>
+    </>
+  );
+}
+
+export default App;
